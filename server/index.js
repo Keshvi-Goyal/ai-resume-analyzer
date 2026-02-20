4dfd8c340c25609c14f1d214133b49ae01882d04
Keshvi Goyal <keshvigoyal62@gmail.com>
2026-02-21 00:04:55 +0530

    Added real skill detection logic

--- server/index.js
+++ server/index.js
@@ -3,6 +3,26 @@ const cors = require("cors");
 const multer = require("multer");
 const pdfParse = require("pdf-parse");
 const fs = require("fs");
+const atsScore = Math.min(
+  Math.round((detectedSkills.length / skillDatabase.length) * 100),
+  100
+);
+const skillDatabase = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Python",
+  "C++",
+  "Java",
+  "SQL",
+  "MongoDB",
+  "HTML",
+  "CSS",
+  "Machine Learning",
+  "Data Structures",
+  "Git",
+  "Express"
+];
 
 const app = express();
 app.use(cors());
@@ -28,20 +48,25 @@ app.post("/upload", upload.single("resume"), async (req, res) => {
     console.log("PDF parsed successfully");
 
     const resumeText = pdfData.text;
+    const detectedSkills = skillDatabase.filter(skill =>
+  resumeText.toLowerCase().includes(skill.toLowerCase())
+   );
 
-    res.json({
-      success: true,
-      message: "Resume analyzed successfully",
-      extractedText: resumeText.slice(0, 500),
-      atsScore: 85,
-      skills: ["JavaScript", "React", "Node"]
-    });
+   res.json({
+  success: true,
+  message: "Resume analyzed successfully",
+  atsScore,
+  skills: detectedSkills,
+  totalSkillsFound: detectedSkills.length
+});
+   
 
   } catch (error) {
     console.error("SERVER ERROR:", error);
     res.status(500).json({ error: error.message });
   }
 });
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
